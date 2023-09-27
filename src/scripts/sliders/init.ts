8574fa4c8fb83ec9f7c1c33bd951d5f3d94d4ad2
Mikelangelo64 <donni2001331@gmail.com>
2023-09-27 16:28:14 +0300

    add catalog section

--- src/scripts/sliders/init.ts
+++ src/scripts/sliders/init.ts
@@ -70,11 +70,49 @@ const sliderFeedbackInit = (sliders: Array<IInitializedSlider>) => {
   });
 };
 
+const sliderCatalogInit = (sliders: Array<IInitializedSlider>) => {
+  const containerArray = document.querySelectorAll(
+    '.catalog'
+  ) as NodeListOf<HTMLElement>;
+
+  if (containerArray.length === 0) {
+    return;
+  }
+
+  containerArray.forEach((item, sliderIndex) => {
+    const slider = makeSlider({
+      container: item,
+      className: 'catalog',
+      paginationType: 'fraction',
+      config: {
+        allowTouchMove: true,
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+
+        breakpoints: {
+          899: {
+            spaceBetween: 24
+          }
+        }
+        // autoplay: {
+        //   delay: 6000,
+        //   disableOnInteraction: false
+        // }
+      }
+    });
+
+    if (slider) {
+      sliders.push({ name: `catalog-${sliderIndex}`, slider });
+    }
+  });
+};
+
 const slidersInit = () => {
   const sliders: Array<IInitializedSlider> = [];
 
   sliderTypesInit(sliders);
   sliderFeedbackInit(sliders);
+  sliderCatalogInit(sliders);
   return sliders;
 };
 
